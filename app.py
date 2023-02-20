ae84668b8faf8e501f69470d655ef26a83264817
AnastasiiaBelianskaia <anastasiiabelianskaia@gmail.com>
2023-02-20 14:00:44 +0200

    Corrected requirements, generate_users and mean functions

--- app.py
+++ app.py
@@ -16,34 +16,29 @@ def main_page():
 def requirements():
     with open('requirements.txt', 'r') as file:
         content = file.read()
-        file.close()
         return render_template("requirements.html", text=content.split('\n')[:-1])
 
 
 @app.route("/generate-users/", methods=['GET'])
 def generate_users():
-    count = request.args.get('count', default='100')
+    count = request.args.get('count', default='100', type=int)
     my_list = []
-    if count.isdigit():
-        for i in range(int(count)):
-            user_name = fake.name()
-            user_email = fake.ascii_free_email()
-            user_info = 'name: ' + user_name + ' email: ' + user_email
-            my_list.append(user_info)
-        return render_template("generate_users.html", list_users=my_list)
-    style_url = url_for('static', filename='style.css')
-    return f"<p><link rel='stylesheet' type='text/css' href={style_url}/></p>Oops, you can't use {count} as a parameter, try to use a number"
+    for i in range(int(count)):
+        user_name = fake.name()
+        user_email = fake.ascii_free_email()
+        user_info = 'name: ' + user_name + ' email: ' + user_email
+        my_list.append(user_info)
+    return render_template("generate_users.html", list_users=my_list)
 
 
 @app.route("/mean/")
 def mean():
-    file = open('hw.csv', 'r')
-    data = list(csv.reader(file, delimiter=','))
+    with open('hw.csv', 'r') as file:
+        data = list(csv.reader(file, delimiter=','))
     height = list(float(row[1]) for row in data[1:] if row)
     weight = list(float(row[2]) for row in data[1:] if row)
     mean_height_cm = (sum(height)/len(height))*2.506
     mean_weight_kg = (sum(weight)/len(weight))/2.205
-    file.close()
     return render_template("mean.html", mhc=mean_height_cm, mwk=mean_weight_kg)
 
 
